c21773ca3d68b3d35c3f8c57d096fa2c1d6028ba
YilmarGarces <gilmar.garces@gmail.com>
2021-12-09 07:45:31 -0500

    cambiando nombre de pagina

--- src/pages/proyectos/index.jsx
+++ src/pages/proyectos/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {GET_PROYECTOS} from 'graphql/proyectos/queries';
 import {Enum_EstadoProyecto,Enum_FaseProyecto} from 'utils/enums';
 
@@ -21,8 +21,6 @@ const IndexProyectos = () => {
             <th>Fecha de finalizacion</th>
             <th>Estado</th>
             <th>Fase</th>
-            
-            
             <th>Editar</th>
           </tr>
         </thead>
@@ -39,11 +37,9 @@ const IndexProyectos = () => {
                   <td>{Enum_FaseProyecto[u.fase]}</td>
                   
                   <td>
-                    {/* <Link to={`/proyectos/editar/${u._id}`}></Link> */}
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                    </svg>
-                    
+                    <Link to={`/proyectos/editar/${u._id}`}>
+                      <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
+                    </Link>
                   </td>
                 </tr>
               );
